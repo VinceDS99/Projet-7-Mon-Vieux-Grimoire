5c613180fce6ce9988899aacf2bf5f37bbf27654
VinceDS99 <vincedos@hotmail.com>
2023-07-03 15:41:36 +0200

    Connexon utilisateur

--- back-end/app.js
+++ back-end/app.js
@@ -44,7 +44,7 @@ app.post('/api/auth/signup', (req, res) =>
 
 //Vérification des informations d'identification del'utilisateur ; renvoie l’_id de l'utilisateur depuis la
 //base de données et un token web JSON signé
-app.post('/api/auth/login', (req, res, next) => 
+app.post('/api/auth/login', (req, res) => 
 {
   User.findOne({ email: req.body.email })
       .then(user => {
@@ -64,26 +64,24 @@ app.post('/api/auth/login', (req, res, next) =>
               .catch(error => res.status(500).json({ error }));
       })
       .catch(error => res.status(500).json({ error }));
-      next();
 });
 
 //Renvoie un tableau de tous les livres de la base de données
-app.get('/api/books', (req, res, next) => {
+app.get('/api/books', (req, res) => {
   Book.find()
     .then(things => res.status(200).json(things))
     .catch(error => res.status(400).json({ error }));
-    next();
 });
 
 //Renvoie le livre avec l’_id fourni.
-app.get('/api/books/:id', (req, res, next) => {
+app.get('/api/books/:id', (req, res) => {
   Thing.findOne({ _id: req.params.id })
     .then(thing => res.status(200).json(thing))
     .catch(error => res.status(404).json({ error }));
 });
 
 //Renvoie un tableau des 3 livres de la base de données ayant la meilleure note moyenne.
-app.get('/api/books/bestrating', (req, res, next) => {
+app.get('/api/books/bestrating', (req, res) => {
   Thing.findOne({ _id: req.params.id })
     .then(thing => res.status(200).json(thing))
     .catch(error => res.status(404).json({ error }));
@@ -92,8 +90,9 @@ app.get('/api/books/bestrating', (req, res, next) => {
 //Capture et enregistre l'image, analyse le livre transformé en chaîne de caractères, et l'enregistre dans la base de données en définissant
 //correctement son ImageUrl.Initialise la note moyenne du livre à 0 et le rating avec un tableau vide. Remarquez que le corps de la
 //demande initiale est vide ; lorsque Multer est ajouté, il renvoie une chaîne pour le corps de la demande en fonction des données soumises avec le fichier.
-app.post('/api/books', (req, res, next) => {
+app.post('/api/books', (req, res) => {
   delete req.body._id;
+  console.log(req.body);
   const book = new Book({
     ...req.body
   });
@@ -103,14 +102,14 @@ app.post('/api/books', (req, res, next) => {
 });
 
 //Met à jour le livre avec l'_id fourni
-app.put('/api/books/:id', (req, res, next) => {
+app.put('/api/books/:id', (req, res) => {
   Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
     .then(() => res.status(200).json({ message: 'Livre modifié !'}))
     .catch(error => res.status(400).json({ error }));
 });
 
 //Supprime le livre avec l'_id fourni ainsi que l’image associée.
-app.delete('/api/books/:id', (req, res, next) => {
+app.delete('/api/books/:id', (req, res) => {
   Book.deleteOne({ _id: req.params.id })
     .then(() => res.status(200).json({ message: 'Livre supprimé !'}))
     .catch(error => res.status(400).json({ error }));
@@ -121,7 +120,7 @@ app.delete('/api/books/:id', (req, res, next) => {
 //tableau "rating" afin de ne pas laisser un utilisateur noter deux fois le même livre.
 //Il n’est pas possible de modifier une note. La note moyenne "averageRating" doit être tenue à
 //jour, et le livre renvoyé en réponse de la requête.
-app.post('/api/books/:id/rating', (req, res, next) => {
+app.post('/api/books/:id/rating', (req, res) => {
   delete req.body._id;
   const book = new Book({
     ...req.body
@@ -130,4 +129,5 @@ app.post('/api/books/:id/rating', (req, res, next) => {
     .then(() => res.status(201).json({ message: 'Livre enregistré !'}))
     .catch(error => res.status(400).json({ error }));
 });
+
 module.exports = app;
--- back-end/models/Book.js
+++ back-end/models/Book.js
@@ -6,8 +6,14 @@ const bookSchema = mongoose.Schema({
     imageUrl: { type: String, required: true },
     year: { type: Number, required: true },
     userId: { type: String, required: true },
-    gender: { type: String, required: true },
-    rating: { type: Number, required: true },
+    genre: { type: String, required: true },
+    ratings: [
+        { 
+            userId : {type: String, required: true },
+            grade : {type: Number, required: true }
+        }
+    ],
+    averageRating: { type: Number, required: false },
 });
 
 module.exports = mongoose.model('Book', bookSchema);
